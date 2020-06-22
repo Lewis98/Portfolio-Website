b6e90cf62c4867eed52c1923136388faff1e5089
Lewis98 <lewis.stokes98@gmail.com>
2020-06-23 00:31:39 +0100

    Added content

--- views/homepage.php
+++ views/homepage.php
@@ -2,19 +2,62 @@
 <img id="backimg" src="resources/img/back1.jpg" alt=""/>
 
 <div class="container">
-<div class="overlay-1">
-    <h1>Freelance Development</h1>
+    
+    <div class="overlay-1">
+        <h1>Freelance Development</h1>
 
-    <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies maximus mauris, 
-    id auctor turpis elementum a. Aliquam laoreet nibh ac enim pellentesque, ut sagittis tortor ultrices.
-     Maecenas sed accumsan arcu, vitae imperdiet orci. Nulla et sapien congue, ultricies metus id, elementum massa. 
-     Maecenas hendrerit gravida nunc, at rhoncus mi. Pellentesque luctus, elit ac blandit vestibulum, 
-     tellus mauris fermentum elit, mollis vestibulum leo quam ut erat. Sed eget nibh eleifend, 
-     eleifend lacus sed, aliquet elit. Sed blandit sapien quis congue lacinia. 
-     Phasellus tempus sapien vitae felis convallis, sit amet viverra neque interdum.
-    </p>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies maximus mauris, 
+        id auctor turpis elementum a. Aliquam laoreet nibh ac enim pellentesque, ut sagittis tortor ultrices.
+        Maecenas sed accumsan arcu, vitae imperdiet orci. Nulla et sapien congue, ultricies metus id, elementum massa. 
+        Maecenas hendrerit gravida nunc, at rhoncus mi. Pellentesque luctus, elit ac blandit vestibulum, 
+        tellus mauris fermentum elit, mollis vestibulum leo quam ut erat. Sed eget nibh eleifend, 
+        eleifend lacus sed, aliquet elit. Sed blandit sapien quis congue lacinia. 
+        Phasellus tempus sapien vitae felis convallis, sit amet viverra neque interdum.
+        </p>
+    </div>
+
+    <div class="whitespace-1">
+    </div>
+
+    <div class="section-1">
+    
+    <h1>Services</h1>
+
+    <div id="hp-flexcont">
+        <a href="/services#webdev">
+            <img class="fleximg" src="resources/ico/dev/webdev.svg">
+            <h1>Web Development</h1>
+        </a>
+
+        <a href="/services#consulting">
+            <img class="fleximg" src="resources/ico/dev/consulting.svg">
+            <h1>Consulting</h1>
+        </a>
+
+        <a href="/services#seo">
+            <img class="fleximg" src="resources/ico/dev/seo.svg">
+            <h1>SEO Optimisation</h1>
+        </a>
+
+        <a href="/services#null">
+            <img class="fleximg" src="resources/ico/dev/tutoring.svg">
+            <h1>Tutoring</h1>
+        </a>
+
+        <a href="/services#null">
+            <img class="fleximg" src="resources/ico/dev/databases.svg">
+            <h1>Databases</h1>
+        </a>
+
+        <a href="/services#null">
+            <img class="fleximg" src="resources/ico/dev/three-gears.svg">
+            <h1>App Development</h1>
+        </a>
+    </div>
+
 </div>
 
-</div>
 
+
+<div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
